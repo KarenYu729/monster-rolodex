9bd33f0043140d036d7795dfb972521b8ac4dbc7
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-07-08 18:51:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter as Router } from ‘react-router-dom’;
-ReactDOM.render(<Router basename={/monster-rolodex}>< App /></Router>, document.getElementById(‘root’));
-
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
