cb4c610aec831ac6a1f77ee73479f4345c5ccde3
Jiahui Yu <97644710+KarenYu729@users.noreply.github.com>
2023-07-08 18:41:10 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter as Router } from ‘react-router-dom’;
+ReactDOM.render(<Router basename={/monster-rolodex}>< App /></Router>, document.getElementById(‘root’));
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
